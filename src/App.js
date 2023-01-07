b9bcbb236d2cd8a5eca62e84c2e92c2ff127cf96
Lucas C. S. Salgado <lucascssalgado@outlook.com>
2023-01-06 22:05:35 -0300

    With these final changes, the calculator now have its functionalities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+
 import Calculator from './components/Calculator';
+import './App.css';
 import './components/Calculator.css';
 import { fontFamily } from '@mui/system';
+import { useEffect } from 'react';
 
 function App() {
+useEffect(() => {
+ const load = setTimeout(() => {
+    return <Calculator/>
+  }, 1000)
+})
+
   return (
     <div className="App">
      <h1 className='header'>Calculator</h1>
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,16 +5,54 @@ import {Box} from "@mui/system";
 
 export default function Calculator() {
     const [num, setNum] = useState(0);
+    const [oldnum, setOldNum] = useState(0);
+    const [operator, setOperator] = useState();
     function inputNum(e){
         let input = e.target.value
         if(input === true){
             num = input
         }
-        setNum(num + input);
+        if(num === 0){
+        setNum(input);
+        }else{
+            setNum(num + input)
+        }
     }   
     function clear(e) {
         setNum(0)
     }
+
+    function percent(){
+        setNum(num/100)
+    }
+
+    function changeSign() {
+        if(num>0) {
+            setNum(-num)
+        }else{
+            setNum(num*-1)
+        }
+    }
+    function calculate() {
+        if(operator === '+'){
+            setNum(parseFloat(oldnum) + parseFloat(num))
+        } else if(operator === '-'){
+            setNum(parseFloat(oldnum) - parseFloat(num))
+        } else if(operator === "*") {
+            setNum(parseFloat(oldnum)* parseFloat(num)) 
+        } else {
+            setNum(parseFloat(oldnum) / parseFloat(num))
+        }
+    }
+
+    function operatorHandler(e){
+        let operatorInput=e.target.value
+        setOperator(operatorInput)
+        setOldNum(num)
+        setNum(0)
+        console.log(num)
+    }
+
     return (
     <div>
         <Box m="5"/>
@@ -23,24 +61,24 @@ export default function Calculator() {
         <div>
             <h1 className="result">{num}</h1>
             <button className="gray" onClick={clear}>AC</button>
-            <button className="gray">+/-</button>
-            <button className="gray">%</button>
-            <button className="gray">/</button>
+            <button className="gray" onClick={changeSign}>+/-</button>
+            <button className="gray" onClick={percent}>%</button>
+            <button className="gray" onClick={operatorHandler} value={"/"}>/</button>
             <button className="darkGrey" onClick={inputNum} value={7}>7</button>
             <button className="darkGrey" onClick={inputNum} value={8}>8</button>
             <button className="darkGrey" onClick={inputNum} value={9}>9</button>
-            <button className="orange">X</button>
+            <button className="orange" onClick={operatorHandler} value={"*"}>X</button>
             <button className="darkGrey"onClick={inputNum} value={4}>4</button>
             <button className="darkGrey" onClick={inputNum} value={5}>5</button>
             <button className="darkGrey" onClick={inputNum} value={6}>6</button>
-            <button className="orange">-</button>
+            <button className="orange" onClick={operatorHandler} value={"-"}>-</button>
             <button className="darkGrey" onClick={inputNum} value={1}>1</button>
             <button className="darkGrey" onClick={inputNum} value={2}>2</button>
             <button className="darkGrey" onClick={inputNum} value={3}>3</button>
-            <button className="orange">+</button>
+            <button className="orange" onClick={operatorHandler} value={"+"}>+</button>
             <button className="darkGrey zero" onClick={inputNum} value={0}>0</button>
-            <button className="darkGrey">,</button>
-            <button className="orange">=</button>
+            <button className="darkGrey" onClick={inputNum} value={"."}>.</button>
+            <button className="orange" onClick={calculate}>=</button>
             </div>
             </Container>
      </div>
